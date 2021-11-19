28769c7d90e95b9ad1b445022880f38fdaa6119e
Joaquín de la Iglesia <joaquindlaiglesia@gmail.com>
2021-11-19 20:09:57 -0300

    Instalado Smarty 3

--- views/zapatillas.view.php
+++ views/zapatillas.view.php
@@ -1,19 +1,21 @@
 <?php
 
+require_once('libs/Smarty.class.php');
 
 class ZapatillasView {
 
     function ListarSneakers($sneakers) {
-        include './templates/header.php';
-        include './templates/form_alta.php';
+        $smarty = new Smarty();
+
+        $smarty->display ('templates/header.tpl');
+        $smarty->display ('templates/form_alta.tpl');
     
-        
         echo '<ul class="list-group mt-5">';
         foreach($sneakers as $sneaker){
             echo "<li class='list-group-item'> Marca: $sneaker->marca Modelo: $sneaker->modelo Color: $sneaker->color Talle US: $sneaker->talle <button class='btn btn-danger'><a href='borrar/$sneaker->id'>Borrar</a></button></li>";}
         echo "</ul>";
     
-        include './templates/footer.php';
+        $smarty->display ('templates/footer.tpl');
     }
 
 }
